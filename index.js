b4d71f2db3c836b87d4c1a0a1599809736c3b0db
GlaceYTT <official.glaceyt@gmail.com>
2025-02-15 13:04:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const { Client, GatewayIntentBits, EmbedBuilder , ActivityType } = require("discord.js");
-
+const path = require('path');
 const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
